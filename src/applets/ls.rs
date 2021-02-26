026da5ca1bb64e9b2639f6b2817b82f659f64516
Efraim Weiss <efiwiss@gmail.com>
2021-02-26 18:50:24 +0200

    add test for short print ls

--- src/applets/ls.rs
+++ src/applets/ls.rs
@@ -117,7 +117,7 @@ fn build_display_fmt(matches: &ArgMatches) -> DisplayFormat {
 #[cfg(test)]
 mod tests {
     use crate::librb::file::filemeta::tests::setup_test;
-    use std::process::Command;
+    use std::process::{Command, Output};
     use super::{_ls_main, subcommand, build_display_fmt};
     use std::ffi::OsStr;
     use std::str;
@@ -145,15 +145,40 @@ mod tests {
         assert!(fmt.color == ColorOption::Auto);
     }
 
+    fn run_cmd(cmd: &str) -> Output {
+        Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .output()
+            .expect("failed to execute process")
+    }
+
     #[test]
     fn test_print_dir() {
         // TODO: Create dir with files and check outputs
         // TODO: Create dir with files and check hidden outputs
         let dir = "/tmp/rustybox-test/ccc";
-        Command::new("sh")
-            .arg("-c")
-            .arg(format!("rm -rf {dir}; mkdir -p {dir}; touch {dir}/a {dir}/b", dir=dir))
-            .output()
-            .expect("failed to execute process");
+        run_cmd(&format!("rm -rf {dir}; mkdir -p {dir}; touch {dir}/a {dir}/b", dir=dir));
+        let args: [&OsStr; 2] = [OsStr::new("ls"), OsStr::new(dir)];
+        let cmd = subcommand();
+        let matches = cmd.get_matches_from(args.iter());
+        let mut output : Vec<u8> = Vec::new();
+        assert!(_ls_main(Some(&matches), &mut output).is_ok());
+        println!("wtf {:?}", output);
+        assert_eq!(str::from_utf8(&output).unwrap(), "b a ");
+    }
+    #[test]
+    fn test_print_dir_hidden_file() {
+        // TODO: Create dir with files and check outputs
+        // TODO: Create dir with files and check hidden outputs
+        let dir = "/tmp/rustybox-test/ccc";
+        run_cmd(&format!("rm -rf {dir}; mkdir -p {dir}; touch {dir}/a {dir}/b {dir}/.c", dir=dir));
+        let args: [&OsStr; 3] = [OsStr::new("ls"), OsStr::new("-a"), OsStr::new(dir)];
+        let cmd = subcommand();
+        let matches = cmd.get_matches_from(args.iter());
+        let mut output : Vec<u8> = Vec::new();
+        assert!(_ls_main(Some(&matches), &mut output).is_ok());
+        println!("wtf {:?}", output);
+        assert_eq!(str::from_utf8(&output).unwrap(), ".c b a ");
     }
 }
